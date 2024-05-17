6a17ac6b395936cc042c7c295b5a314330c7ad07
Thibaud MARLIER <thibaud.marlier@algosup.com>
2024-05-17 10:11:50 +0200

    Setting the coding environment

---
+++ adopte_a_candidate/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello you'),
+        ),
+      ),
+    );
+  }
+}
