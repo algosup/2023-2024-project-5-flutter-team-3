8ce5b77211d93825b9817f2b8f7505b3f0a49b03
Biohazardyee <146005163+Biohazardyee@users.noreply.github.com>
2024-06-10 19:33:08 +0200

    Added new functionalities to the profile page
    The user can now delete skills that he doesn't wish to have on his profile anymore by pressing the cross
    
    The data is stored until the app is restarted, meaning information stay on the profile page even when changing page

--- adopte_a_candidate/lib/services/providers/providers.dart
+++ adopte_a_candidate/lib/services/providers/providers.dart
@@ -4,8 +4,13 @@ class ProfileState extends ChangeNotifier {
   String _aboutMeText = '';
   final List<String> _aboutMeTexts = [];
   final TextEditingController aboutMeController = TextEditingController();
+  final List<String> _mainSkills = [];
+  final List<String> _sideSkills = [];
 
   String get aboutMeText => _aboutMeText;
+  List<String> get mainSkills => _mainSkills;
+  List<String> get sideSkills => _sideSkills;
+
   List<String> get aboutMeTexts => _aboutMeTexts;
 
   void updateAboutMeText(String newText) {
@@ -16,6 +21,31 @@ class ProfileState extends ChangeNotifier {
   void addAboutMeText(String newText) {
     _aboutMeTexts.clear();
     _aboutMeTexts.add(newText);
+    notifyListeners();
+  }
+
+
+  void addMainSkill(String skill) {
+    if (_mainSkills.length < 5 && !_mainSkills.contains(skill)) {
+      _mainSkills.add(skill);
+      notifyListeners();
+    }
+  }
+
+  void removeMainSkill(String skill) {
+    _mainSkills.remove(skill);
+    notifyListeners();
+  }
+
+  void addSideSkill(String skill) {
+    if (_sideSkills.length < 10 && !_sideSkills.contains(skill)) {
+      _sideSkills.add(skill);
+      notifyListeners();
+    }
+  }
+
+  void removeSideSkill(String skill) {
+    _sideSkills.remove(skill);
     notifyListeners();
   }
 }
