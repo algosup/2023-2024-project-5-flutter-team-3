836d841b699ab269586eea5676c80a8c186a17c0
Lucas Aubard <lucas.aubard@algosup.com>
2024-06-10 11:54:44 +0200

    Update term_of_use.dart

--- adopte_a_candidate/lib/pages/term_of_use.dart
+++ adopte_a_candidate/lib/pages/term_of_use.dart
@@ -13,10 +13,33 @@ class TermOfUsePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const Logo(),
-      body: const Text('Term of use'),
+      appBar: AppBar(
+        title: const Text('Term of Usage'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            context.goNamed('settings');
+          },
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+              'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+              'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+              'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+              'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+              'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+              'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+              'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+              'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+        ),
+      ),
       bottomNavigationBar: CustomBottomNavBar(
-        currentRoute: '/term_of_use', // Set the current route for MessagePage
+        currentRoute: '/term_of_use', // Set the current route for TermOfUsePage
         onItemTapped: (index) {
           switch (index) {
             case 0:
@@ -26,7 +49,7 @@ class TermOfUsePage extends StatelessWidget {
               context.goNamed('swipe'); // Navigate to swipe page
               break;
             case 2:
-              // Already on the message page, no navigation needed
+              // Already on the term of use page, no navigation needed
               break;
             default:
               break;
